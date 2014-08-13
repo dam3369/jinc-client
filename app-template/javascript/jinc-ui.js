aa439a15bbb4f95019e46d689d9cd717dac06623
Nicolas Dufreche <n.dufreche@gmail.com>
2014-08-13 09:12:05 +0200

    Update ui and cleaning stylesheet

--- app-template/javascript/jinc-ui.js
+++ app-template/javascript/jinc-ui.js
@@ -29,43 +29,66 @@
             return number;
         };
 
-    window.printDistance = function (distance) {
-        distance = Math.round(distance);
+    window.ui = {
+        printDistance: function (distance) {
+            distance = Math.round(distance);
 
-        if (distance === undefined && element.number.dataset.value !== undefined) {
-            distance = element.number.dataset.value
-        } else if (distance === undefined) {
-            distance = 0;
-        }
-        element.number.dataset.value = distance;
+            if (distance === undefined && element.number.dataset.value !== undefined) {
+                distance = element.number.dataset.value
+            } else if (distance === undefined) {
+                distance = 0;
+            }
+            element.number.dataset.value = distance;
 
-        if (!isMetricSytem()) {
-            distance = convertToMilles(distance);
+            if (!isMetricSytem()) {
+                distance = convertToMilles(distance);
+            }
+            if (distance > 999) {
+                distance = upgradeUnit(distance);
+            }
+            if (isNaN(distance)) {
+                distance = 0;
+            }
+            element.number.innerHTML = distance;
+        },
+        changeUnit: function () {
+            if (isMetricSytem()) {
+                element.unit.innerHTML = 'yd';
+                element.unit.dataset.unit = 'datum'
+            } else {
+                element.unit.innerHTML = 'm';
+                element.unit.dataset.unit = 'metric'
+            }
+            ui.printDistance();
+        },
+        onResizeWindow: function () {
+            var height = document.body.clientHeight,
+                width = document.body.clientWidth,
+                margintop = 0;
+
+            if (height > width) {
+                margintop = (height - ((width/100)*80)) / 2;
+            } else {
+                margintop = (height - ((height/100)*80)) / 2;
+            }
+            document.getElementById('arrow').style.marginTop = margintop.toString() + 'px';
+        },
+        setActivity: function (activ) {
+            if (activ) {
+                element.wallpaper.classList.remove('inactive');
+                element.wallpaper.classList.add('active');
+            } else {
+                element.wallpaper.classList.remove('active');
+                element.wallpaper.classList.add('inactive');
+            }
         }
-        if (distance > 999) {
-            distance = upgradeUnit(distance);
-        }
-        element.number.innerHTML = distance;
     };
 
-    window.arrow = element.arrow;
-
-    window.arrow.rotate = function (radiant) {
+    ui.arrow = element.arrow;
+    ui.arrow.rotate = function (radiant) {
         element.arrow.style.transform = 'rotate(' + radiant + 'rad)';
     };
-
-    window.arrow.center = function () {
+    ui.arrow.center = function () {
         rotateArrow(0);
-    };
-
-    window.changeUnit = function () {
-        if (isMetricSytem()) {
-            element.unit.innerHTML = 'yd';
-            element.unit.dataset.unit = 'datum'
-        } else {
-            element.unit.innerHTML = 'm';
-            element.unit.dataset.unit = 'metric'
-        }
-        printDistance();
     };
 } (window));
